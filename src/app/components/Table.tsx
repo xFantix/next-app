faab705430f9376e7ed58684853926aee60302ec
Kamil <kamil.jankoowski@outlook.com>
2025-08-18 15:05:58 +0200

    small change

--- src/app/components/Table.tsx
+++ src/app/components/Table.tsx
@@ -1,14 +1,11 @@
 import { getProducts } from '@/service/product.service';
-import { Suspense } from 'react';
 import ProductTable from './ProductTable';
 export default async function Table() {
   const products = await getProducts();
 
   return (
     <div className="flex flex-col gap-4">
-      <Suspense fallback={<div>Loading...</div>}>
-        <ProductTable products={products} />
-      </Suspense>
+      <ProductTable products={products} />
     </div>
   );
 }
